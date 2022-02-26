a9bb94402f954eacc5674df7c767642725cdc715
Manoj <b8038312@my.shu.ac.uk>
2022-02-26 17:28:37 +0000

    Added more complex movements using angles
    Also updated readme to include what string inputs the zumo is looking for

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # ZumoSearchAndRescue
 ProgrammingThings Assignment
 
+## Set up of the Zumo
+- After uploading the ino file, press the A button on the zumo to begin calibration
+
+## Manual inputs for zumo 
+- 'm' starts manual control for zumo
+- 'w' zumo moves forward for 200 milliseconds
+- 'a' zumo moves left for 200 milliseconds
+- 's' zumo moves backwards for 200 milliseconds
+- 'd' zumo moves right for 200 milliseconds
+- 'q' zumo turns 90 degrees to the left
+- 'e' zumo turns 90 degrees to the right
+- 'u' zumo u-turns 180 degrees from the right
+- 'y' zumo u-turns 360 degrees from the right
 
 github link - https://github.com/ManojKangokar/ZumoSearchAndRescue
--- SearchAndRescue/SearchAndRescue.ino
+++ SearchAndRescue/SearchAndRescue.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
-const uint16_t maxSpeed = 400;
+#include "TurnSensor.h"
+
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4Buzzer buzzer;
 Zumo32U4LineSensors lineSensors;
+L3G gyro;
 
 #define NUM_SENSORS 5
-#define speed 150
+#define speed 180
+#define turnSpeed 150
 
 unsigned int lineSensorValues[NUM_SENSORS];
 
@@ -39,44 +42,83 @@ void calibrateSensors()
   }
 }
 
+void turnLeft(int degrees) {
+  turnSensorReset();
+  motors.setSpeeds(-turnSpeed, turnSpeed);
+  int angle = 0;
+  do {
+    delay(1);
+    turnSensorUpdate();
+    angle = (((int32_t)turnAngle >> 16) * 360) >> 16;
+  } while (angle < degrees);
+  motors.setSpeeds(0, 0);
+}
+
+void turnRight(int degrees) {
+  turnSensorReset();
+  motors.setSpeeds(turnSpeed, -turnSpeed);
+  int angle = 0;
+  do {
+    delay(1);
+    turnSensorUpdate();
+    angle = (((int32_t)turnAngle >> 16) * 360) >> 16;
+  } while (angle > -degrees);
+  motors.setSpeeds(0, 0);
+}
+
 void ManualControl(){
   input = Serial1.read();
 
   switch(input){
-    case 'w':
+    case 'w': // forward
       motors.setSpeeds(speed,speed);
       delay(200);
       motors.setSpeeds(0,0);
       break;
-    case 'a':
+    case 'a': // left
       motors.setSpeeds(speed * -1,speed);
       delay(200);
       motors.setSpeeds(0,0);
       break;
-    case 's':
+    case 's': // backwards
       motors.setSpeeds(speed * -1,speed * -1);
       delay(200);
       motors.setSpeeds(0,0);
       break;
-    case 'd':
+    case 'd': // right
       motors.setSpeeds(speed,speed * -1);
       delay(200);
       motors.setSpeeds(0,0);
+      break;
+    case 'q': // 90 degrees left
+      turnLeft(90);
+      break;
+    case 'e': // 90 degrees right
+      turnRight(90);
+      break;
+    case 'u': // 180 degrees - u turn
+      turnRight(180);
+      break;
+    case 'y'  // 360 degrees - turn
+      turnRight(360);
       break;
   }
 }
 
 void setup() {
+  // TODO: set up all sensors
   Serial1.begin(9600);
   lineSensors.initFiveSensors();  // inilizes 5 sensors 
   buttonA.waitForButton(); // for safety so motors dont turn until A hasnt been clicked 
   calibrateSensors(); // calabrates sensors 
   control = 'm'; // setting control to manual
-
-  // make sure you set up all sensors 
+  turnSensorSetup();  // setting up gyro for turning set no of degrees
+  delay(500);
+  turnSensorReset(); // for calibration
 }
 
 void loop() {
+  turnSensorUpdate(); // start of every loop the angle is updated 
   switch(control){
     case 'm': 
     ManualControl();
